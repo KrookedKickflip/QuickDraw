a13defbf83f192eb431b55a6668a859b2d720682
KrookedKickflip <colinnicosia84@yahoo.com>
2023-01-04 18:19:58 -0600

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,44 @@ var timerCount;
 var lettersInChosenWord = [];
 var blanksLetters = [];
 var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+var input = document.querySelector("imput");
+var resetButton = document.querySelector("reset-button")
+
+resetButton.addEventListener("click", resetScore)
+
+function resetScore() {
+    winCounter = 0;
+    loseCounter = 0;
+}
+
+chosenWord = words[Math.floor(Math.random() * words.length)];
+lettersInChosenWord = chosenWord.split("");
+numBlanks = lettersInChosenWord.length;
+
+for (var i = 0; i < numBlanks; i++) {
+    blanksLetters.push("_");
+  }
+
+wordBlank.textContent = blanksLetters.join(" ");
+startButton.addEventListener("click", startTimer);
+function startTimer() {
+    // Reset the timer and isWin variable
+    timerCount = 15;
+    isWin = false;
+  
+    // Display the timer
+    timerElement.textContent = timerCount;
+  
+    // Start the timer
+    timer = setInterval(function() {
+      timerCount--; // Decrement the timer by 1 second
+      timerElement.textContent = timerCount;
+      if (timerCount == 0) {
+        clearInterval(timer);
+        loseGame();
+      } // Update the timer display
+    }, 1000);
+}
 
 // create a function that runs when the page loads
 function loadPage()
@@ -21,7 +59,15 @@ function startGame()
 // create a winGame function that runs when the win condition is met
 function winGame()
 // create a loseGame function that runs with timer reaches 0
-function loseGame()
+function loseGame() {
+    // Code to run when the timer reaches 0 goes here
+  }
+  
+   
+  
+  
+  startTimer(); // Start the timer
+  
 // create a setTimer function that starts and stops the timer and triggers winGame() or loseGame()
 function setTimer()
 // create a function that creates blanks on the screen that correspond to the number of letters in the word that's to be guessed
@@ -38,7 +84,7 @@ function setWin() {
     winCounter.innerHTML = winCount;
   }
 // create a function that updates losses on screen and sets loss count to local storage
-function setLoss()
+function setLoss() 
 // create a function that checks if the player as won, if so set the isWin variable to true
 
 // create a function that tests if the guessed letter is in the word and renders it to the screen
@@ -46,3 +92,4 @@ function setLoss()
 // attach a keydown event listener to run the checkLetters function and the checkWin function;
 
 // attach a click event listener to run the startGame function
+    
